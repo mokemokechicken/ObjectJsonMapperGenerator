5cf5c6d0ee084a3995b662ef238038d8e286eee5
Ken Morishita <k_morishita@yumemi.co.jp>
2014-12-08 18:09:54 +0900

    a

--- lib/API/swift/swift_api_generator.rb
+++ lib/API/swift/swift_api_generator.rb
@@ -3,7 +3,7 @@ module Yousei::APIGenerator
     class SwiftGenerator < GeneratorBase
       def create_entity
         generator = Yousei::OJMGenerator::Swift::SwiftOJMGenerator.new
-        generator.generate @entity_def
+        generator.generate @entity_def, namespace: @entity_prefix.to_s
       end
 
       def create_api
